71c2464d29274f139e33e80ff0b4bac841ed6771
Matthew Taylor <majikpig@gmail.com>
2020-05-17 20:07:43 -0400

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,51 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import Logout from "./components/Logout";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
-function App() {
+function App(props) {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    if (localStorage.getItem("token")) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  },[])
+
+  const login = () => {
+    setLoggedIn(true);
+  };
+
+  const logout = () => {
+    setLoggedIn(false);
+  };
+
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
+        {/* {<div class="topnav">
+          <h1>Welcome to the Bubble App!</h1>
+          {!loggedIn?<NavLink to="/" class="active">Bubble App</NavLink>:<NavLink to="/bubbles" class="active">Bubble App</NavLink>}
+          <div className="myLinks hidden" id="myLinks">
+            {loggedIn?<Link to="/logout">Logout</Link>:<Link to="/login">Login</Link>}
+          </div>
+        </div>} */}
+        <Switch>
+          <PrivateRoute exact path="/bubbles" component={BubblePage} />
+          <Route path="/login" render={(props)=> <Login {...props} func={login} />}/>
+          <Route exact path="/" render={(props)=> <Login {...props} func={login} />}/>
+          <Route path="/logout" render={(props)=> <Logout {...props} history={props.history} func={logout} />}/>
+          {!loggedIn?<Route render={(props)=> <Login {...props} func={login} />}/>:<PrivateRoute component={BubblePage} />}
+        </Switch>
+        {/*
+          Build a PrivateRoute component that will
+          display BubblePage when you're authenticated
         */}
       </div>
     </Router>
